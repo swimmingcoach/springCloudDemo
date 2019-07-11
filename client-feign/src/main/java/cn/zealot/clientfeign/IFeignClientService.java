533a22c5356f38ee7f1d665db37d07bb3aa9e395
swimmingcoach <zshang_0630@sina.com>
2019-07-11 10:18:18 +0800

    引入第二个服务提供者，名称与第一个服务提供者保持一致，消费者基于名称消费服务的时候会自动负载均衡

--- client-feign/src/main/java/cn/zealot/clientfeign/IFeignClientService.java
+++ client-feign/src/main/java/cn/zealot/clientfeign/IFeignClientService.java
@@ -3,12 +3,13 @@ package cn.zealot.clientfeign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
 @FeignClient(value = "service-hello") //这里的name对应调用服务的spring.applicatoin.name
 public interface IFeignClientService {
 
-    @RequestMapping(value = "/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     String hello(@RequestParam("id") String id);
 }
